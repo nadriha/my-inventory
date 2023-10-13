d55f2e3afff2ca8af34891cdd8b8202c50abd4e5
nadriha <hafez.nadhira@gmail.com>
2023-10-13 11:48:10 +0700

    tugas 6

--- my_inventory/settings.py
+++ my_inventory/settings.py
@@ -88,6 +88,7 @@ if PRODUCTION:
         'default': env.db('DATABASE_URL')
     }
     DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
